5b8a42516b1c0f38d03e53c8146800f1d48579c4
demskie <alex.demskie@gmail.com>
2020-03-01 05:14:21 -0600

    Updates

---
+++ precache-manifest.b25bc2efe840b4897d541e2613dca96d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f92ca46ae0c9332cb620b5a78cb0689",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c52b02cb86e85d6cdf70",
+    "url": "/static/css/main.789974f3.chunk.css"
+  },
+  {
+    "revision": "c7d8bf0e7cc0d6ac731a",
+    "url": "/static/js/2.78e545ed.chunk.js"
+  },
+  {
+    "revision": "bddc389d1f0acfe3fcf54d77b334132a",
+    "url": "/static/js/2.78e545ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c52b02cb86e85d6cdf70",
+    "url": "/static/js/main.b79119c6.chunk.js"
+  },
+  {
+    "revision": "739b60e8249a386677b6",
+    "url": "/static/js/runtime-main.84b12433.js"
+  }
+]);
